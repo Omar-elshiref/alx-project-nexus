bc9a0eebbeb344b4deb9b1066469a947bb3632f2
Omar mohamed <omarabdou2002@yahoo.com>
2025-08-10 17:23:11 +0300

    feat: Add wishlist functionality with product display and reset option

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -264,6 +264,32 @@ const CartPage = () => {
                 <div className="md:hidden fixed bottom-0 left-0 w-full bg-white pt-2">
                   <div className="bg-white p-4 rounded-lg border mx-4">
                     <h2>Order Summary</h2>
+                          <div className="space-y-4">
+                        <div className="flex items-center justify-between">
+                          <span>SubTotal</span>
+                          <PriceFormatter amount={getSubTotalPrice()} />
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <span>Discount</span>
+                          <PriceFormatter
+                            amount={getSubTotalPrice() - getTotalPrice()}
+                          />
+                        </div>
+                        <Separator />
+                        <div className="flex items-center justify-between font-semibold text-lg">
+                          <span>Total</span>
+                          <PriceFormatter
+                            amount={getTotalPrice()}
+                            className="text-lg font-bold text-black"
+                          />
+                        </div>
+                        <Button
+                          className="w-full rounded-full font-semibold tracking-wide hoverEffect"
+                          size="lg"
+                        >
+                          {loading ? "Please wait..." : "Proceed to Checkout"}
+                        </Button>
+                      </div>
                   </div>
                 </div>
               </div>
