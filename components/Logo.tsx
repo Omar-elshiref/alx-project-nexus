874576397160d6b2fe19b9e7a0a2c31e4d4aba1c
Omar mohamed <omarabdou2002@yahoo.com>
2025-08-11 00:55:06 +0300

    Done

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = ({ className, spanDes }: { className?: string, spanDes?: string}) =
   return (
     <Link href={"/"} className="inline-flex">
         <h2 className={cn("text-2xl text-shop_dark_green font-black tracking-wider uppercase cursor-pointer hover:text-shop_light_green hoverEffect group font-sans", className)}>
-            Shopcar<span className={cn("text-shop_light_green group-hover:text-shop_dark_green hoverEffect", spanDes)}>t</span>
+            Nexus<span className={cn("text-shop_light_green group-hover:text-shop_dark_green hoverEffect", spanDes)}> Shop</span>
         </h2>
     </Link>
   )
